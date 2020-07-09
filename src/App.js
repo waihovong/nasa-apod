323d0b38a9d51bf05a55ce42d1a72cd5e0c8ecdf
Wai Ho Vong <waihovong@Wais-Air.lan>
2020-07-09 16:43:04 +0930

    designed astronomy picture of the day using nasa api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import AstroPic  from '../src/components/AstroPic';
 import './App.css';
 
-function App() {
+import { BrowserRouter } from 'react-router-dom'; 
+require('dotenv').config()
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="home">
+        <AstroPic />
+      </div>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/components/AstroPic.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+
+const apiKey = process.env.REACT_APP_API_KEY;
+console.log(apiKey)
+
+export default function Apod() {
+    const [photoData, setPhotoData] = useState(null);
+    useEffect(()=> {
+        fetchPhoto();
+
+        async function fetchPhoto() {
+            const res = await fetch(
+            `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+        );
+            const data = await res.json();
+            setPhotoData(data);
+            console.log(data);
+        }
+    }, []);
+    if(!photoData) return <div/>
+    return (
+        <div className="apod-container">
+            <div className="nasa-photo nasa-item">
+                <h1 className="title">Astronomy Picture of the Day</h1>
+                <img
+                    src={photoData.url}
+                    alt={photoData.title}
+                    className="photo"
+                    />
+            </div>
+            <div className="nasa-data nasa-item">
+                <h1 className="title">{photoData.title}</h1>
+                <p className="date">{photoData.date}</p>
+                <p className="author">{photoData.copyright}</p>
+                <p className="explanation">{photoData.explanation}</p>
+            </div>
+        </div>
+    );
+}
+
